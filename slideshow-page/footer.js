4f917c19917f305cdf2c3b86f7492c6ec16fa644
5yeus <silas.owu@amalitech.com>
2023-05-07 18:07:44 -0700

    Mobile UI Slideshow page updated

--- slideshow-page/footer.js
+++ slideshow-page/footer.js
@@ -1,6 +1,21 @@
 document.querySelector('#footer').innerHTML = `
 
-    <div class="bg-[#5ca]">
-    
+    <div class=" flex flex-col">
+        <div class="bg-mercury w-full h-[0.0625rem]"></div>
+        <div class="flex flex-row mobile:mx-6  mobile:gap-[10.5rem]" >
+            <div class="mobile:w-[5.8125rem] mobile:h-[2.375rem] bg-[#b9A]"></div>
+            <div class="mobile:w-[3.55rem] mobile:h-[1rem] self-center flex flex-row mobile:gap-6">
+                <div class="fill-none">
+                    <svg width="26" height="24" xmlns="http://www.w3.org/2000/svg"><g stroke="#000" fill="none" fill-rule="evenodd"><path d="M24.166 1.843L3.627 12.113l20.539 10.269V1.843z" stroke-width="2"/>
+                        <path fill="#D8D8D8" d="M.986.5h-1v22.775h1z"/></g>
+                    </svg>
+                </div>
+                <div>
+                    <svg width="26" height="24" xmlns="http://www.w3.org/2000/svg"><g stroke="#000" fill="none" fill-rule="evenodd">
+                        <path d="M1.528 1.843l20.538 10.27L1.528 22.382V1.843z" stroke-width="2"/><path fill="#D8D8D8" d="M24.708.5h1v22.775h-1z"/></g>
+                    </svg>
+                </div>
+            </div>
+        </div>
     </div>
 `;
