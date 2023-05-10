76c4c0957f23331726662eb8c95f1727582dc6c4
5yeus <silas.owu@amalitech.com>
2023-05-09 21:23:29 -0700

    Homepage updated

---
+++ slideshow-page/slideshow-helper.js
@@ -0,0 +1,9 @@
+$(function(){
+    fetch('../public/data.json')
+      .then( response => response.json())
+      .then( data => {
+        const galleriaData = data;
+        console.log(galleriaData);
+      })
+     .catch(err=>{console.log(err)}); 
+});
